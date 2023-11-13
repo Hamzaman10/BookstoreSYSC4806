816104aea02a38e70b50feb1deca005cbbff0a19
kyler <vergekyler@gmail.com>
2023-11-12 19:02:44 -0500

    index has owner creator

--- src/main/java/controller/OwnerController.java
+++ src/main/java/controller/OwnerController.java
@@ -13,7 +13,7 @@ public class OwnerController {
     private BookStoreManagementRepository bookStoreManagementRepository;
 
 
-    @PostMapping("/createOwner")
+    @PostMapping
     public Owner createOwner(@RequestBody Owner newOwner){
 
         BookStoreManagement ownerBookStore = new BookStoreManagement();
